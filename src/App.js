525fbf78c33432ce4603b464dd16f0ad088915c4
C109156123 <82920051+C109156123@users.noreply.github.com>
2023-03-02 11:21:51 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Tiawan</h1>
+      <p>Hello NKUST!!</p>
+      <img className='App-logo' src='logo.svg'></img>
+      
+    </div>
+    
+  );
+}
+
+export default App;
